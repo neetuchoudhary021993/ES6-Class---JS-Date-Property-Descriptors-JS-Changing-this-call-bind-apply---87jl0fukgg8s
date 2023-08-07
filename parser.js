c6ec6a1760c8b29f5401a47b5a2c3c16de63b4a7
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-08-07 22:35:57 +0530

    parser.js

--- parser.js
+++ parser.js
@@ -1,3 +1,35 @@
+class API {
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#setSecure();
+  }
+
+  #setSecure() {
+    this.#secure = this.url.startsWith('https');
+  }
+
+  isSecure() {
+    return this.#secure;
+  }
+
+  updateURL(newURL) {
+    this.url = newURL;
+    this.#setSecure();
+  }
+}
+
+// Example usage:
+// const s = new API('http://api.com/api/hello');
+// console.log(s.isSecure()); // false
+
+// s.updateURL('https://api.com/api/hello');
+// console.log(s.isSecure()); // true
+
+// console.log(s.url); // https://api.com/api/hello
+
+
+
 var parser = require('xml-js');
 
 const xml = require('fs').readFileSync('junit.xml', 'utf-8');
