c174383d0b648bd3fe8d3f2a496d1754992848f9
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-08-07 22:37:15 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 //Write your code here
+class API {
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#setSecure();
+  }
 
+  #setSecure() {
+    this.#secure = this.url.startsWith('https');
+  }
 
+  isSecure() {
+    return this.#secure;
+  }
 
+  updateURL(newURL) {
+    this.url = newURL;
+    this.#setSecure();
+  }
+}
+
+// // Example usage:
+// const s = new API('http://api.com/api/hello');
+// console.log(s.isSecure()); // false
+
+// s.updateURL('https://api.com/api/hello');
+// console.log(s.isSecure()); // true
+
+// console.log(s.url); // https://api.com/api/hello
 
 
 //Do not remove this code
